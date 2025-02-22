bb0f34e61fc3d2d83b09ef1fa1a5504b0c74a587
BBP090 <141896021+BBP090@users.noreply.github.com>
2025-02-22 20:15:49 +0545

    setup

---
+++ build/CMakeFiles/traffic-signal.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bbp/dsa/traffic-signal/simulator.c" "CMakeFiles/traffic-signal.dir/simulator.c.o" "gcc" "CMakeFiles/traffic-signal.dir/simulator.c.o.d"
+  "/home/bbp/dsa/traffic-signal/trafficGenerator.c" "CMakeFiles/traffic-signal.dir/trafficGenerator.c.o" "gcc" "CMakeFiles/traffic-signal.dir/trafficGenerator.c.o.d"
+  "" "traffic-signal" "gcc" "CMakeFiles/traffic-signal.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
